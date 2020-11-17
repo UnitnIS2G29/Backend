53a79177a2f5dafd1b07ed051db9348a4e6bc443
GabrieleStulzer <gabriele.stulzer@gmail.com>
2020-11-17 08:22:20 +0100

    Add base auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,22 @@
+const JWT = require('jsonwebtoken');
+const User = require('../database/models/user');
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace('Bearer ', '');
+    const data = JWT.verify(token, process.env.AUTHKEY);
+    const user = await User.findOne({_id: data.id, 'tokens.token': token});
+
+    if(!user)
+      res.status(401).send(new Error("Not Authorized User"));
+
+    req.user = user;
+
+    next();
+
+  } catch (e) {
+    res.status(500).send(e);
+  }
+}
+
+module.exports = auth;
