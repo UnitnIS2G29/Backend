96443241cdf0f1c0683711c93c785dd34108477e
Alessandro Visintainer <github@alexdev.it>
2020-11-18 17:52:35 +0100

    Check email and password instead of name

--- routes/login.js
+++ routes/login.js
@@ -33,7 +33,7 @@ router.post('/', [
     try {
       console.log("Retrieving User");
       console.log(req.body);
-      const user = await User.findOne({name: req.body.name}).exec();
+      const user = await User.findOne({email: req.body.email,password: req.body.password}).exec();
       console.log(user);
       if(!user) {
         res.status(404).send(new Error("User not found"));
