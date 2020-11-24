8e680734cc1f0c11159cbabb5f86cb852e9b897e
GabrieleStulzer <gabriele.stulzer@gmail.com>
2020-11-24 20:41:46 +0100

    Add port config and Proc file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-const port = 3000;
+const port = process.env.PORT ||  3000;
 
 const dbOptions = require('./database/dbConst');
 
