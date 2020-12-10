237cd5c91d983eef562db20be4d630a8431b86a9
GabrieleStulzer <gabriele.stulzer@gmail.com>
2020-12-10 10:48:59 +0100

    Add test authorization error code

--- test/logout.test.js
+++ test/logout.test.js
@@ -24,12 +24,25 @@ describe("Logout testing", () => {
 
   // Test endpoint availability
 
-  test("DELETE /authentications ~Tes endpoint working~", async done => {
+  test("DELETE /authentications ~Test endpoint working~", async done => {
     let res = await request(app).delete('/authentications')
       .set("Authorization", `Bearer ${token}`)
       .expect(200)
 
     expect(res.tokens).toBeFalsy();
+
+    done();
+  });
+
+  // Test Authorization Error Code
+
+  test("DELETE /authentications ~Test Authorization Error Code~", async done => {
+    await request(app).delete('/authentications')
+      .set("Authorization", `Bearer ${token}`)
+
+    await request(app).get('/authentications')
+      .set("Authorization", `Bearer ${token}`)
+      .expect(401)
 
     done();
   });
