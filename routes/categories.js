a7cadb099aa7d4b3215f5b6168f00475c995324b
Alessandro Visintainer <github@alexdev.it>
2020-12-12 19:12:02 +0100

    Fixato debugDB in categories

--- routes/categories.js
+++ routes/categories.js
@@ -26,7 +26,6 @@ router.get('/:id', auth(), async (req, res) => {
     const category = await Category.findById(req.params.id);
     res.send(category);
   } catch (e) {
-    debugDB(e);
     return res.status(500).send({ error: e.message });
   }
 });
