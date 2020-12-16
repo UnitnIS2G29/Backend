b8e159f83e222cad9265e73196f4826f1e42a14c
Marco Zamboni <mazamboni@fbk.eu>
2020-12-16 16:44:12 +0100

    Add some tests

--- test/requestTimeOff.test.js
+++ test/requestTimeOff.test.js
@@ -7,21 +7,32 @@ const app = require("../app");
 
 
 describe("requestTimeOff testing", () => {
-    let user;
-    let token;
+    let supervisor;
+    let super_token;
+    let employee;
+    let employee_token;
 
     setupDB("requestTimeOff_testing");
 
     beforeEach(async () => {
-        let userObj = {
+        let superObj = {
             name: "Supervisor to Perform Actions",
             email: "testuser@test.com",
             role: "supervisor",
             password: "12345678",
         };
-        user = new User(userObj);
-        user = await user.save();
-        token = await user.generateToken();
+        supervisor = new User(superObj);
+        supervisor = await supervisor.save();
+        super_token = await supervisor.generateToken();
+        let empObj = {
+            name: "Basic employee",
+            email: "testemp@test.com",
+            role: "employee",
+            password: "12345",
+        };
+        employee = new User(empObj);
+        employee = await employee.save();
+        employee_token = await employee.generateToken();
     });
 
 
@@ -33,7 +44,7 @@ describe("requestTimeOff testing", () => {
             timeEnd: "2021-12-14T18:00:00.000Z",
             category: "illness"   
         })
-        .set("Authorization", `Bearer ${token}`)
+        .set("Authorization", `Bearer ${super_token}`)
         .expect(201)
         done();
     });
@@ -46,7 +57,7 @@ describe("requestTimeOff testing", () => {
             timeEnd: "14/12/2021 6:00 PM",
             category: "illness"   
         })
-        .set("Authorization", `Bearer ${token}`)
+        .set("Authorization", `Bearer ${super_token}`)
         .expect(400)
         done();
     });
@@ -59,7 +70,7 @@ describe("requestTimeOff testing", () => {
             timeEnd: "2021-12-14T11:00:00.000Z",
             category: "illness"   
         })
-        .set("Authorization", `Bearer ${token}`)
+        .set("Authorization", `Bearer ${super_token}`)
         .expect(400)
         done();
     });
@@ -72,8 +83,49 @@ describe("requestTimeOff testing", () => {
             timeEnd: "2021-12-14T18:00:00.000Z",
             category: "illness"   
         })
-        .set("Authorization", `Bearer ${token}`)
+        .set("Authorization", `Bearer ${super_token}`)
         .expect(400)
+        done();
+    });
+
+    test("GET all RequestTimeOff: success",async done => {
+        const resPost = await request(app).post('/requestTimeOff')
+            .send({
+                day: "2021-12-14T13:00:00.000Z",
+                timeBegin: "2021-12-14T13:00:00.000Z",
+                timeEnd: "2021-12-14T18:00:00.000Z",
+                category: "illness"
+            })
+            .set("Authorization", `Bearer ${super_token}`)
+            .expect(201)
+
+        const resGetSelf = await request(app).get('/requestTimeOff')
+            .set("Authorization", `Bearer ${super_token}`)
+            .expect(200)
+            .expect((data) => {
+                expect(data.body[0].day).toEqual(resPost.body.day);
+                expect(data.body[0].timeBegin).toEqual(resPost.body.timeBegin);
+                expect(data.body[0].timeEnd).toEqual(resPost.body.timeEnd);
+                expect(data.body[0].category).toEqual(resPost.body.category);
+                expect(data.body[0].reviewed).toEqual(false);
+            });
+        done();
+    });
+
+    test("GET all RequestTimeOff: unauthorized",async done => {
+        const resPost = await request(app).post('/requestTimeOff')
+            .send({
+                day: "2021-12-14T13:00:00.000Z",
+                timeBegin: "2021-12-14T13:00:00.000Z",
+                timeEnd: "2021-12-14T18:00:00.000Z",
+                category: "illness"
+            })
+            .set("Authorization", `Bearer ${super_token}`)
+            .expect(201)
+
+        const resGetSelf = await request(app).get('/requestTimeOff')
+            .set("Authorization", `Bearer ${employee_token}`)
+            .expect(401)
         done();
     });
 
@@ -85,11 +137,11 @@ describe("requestTimeOff testing", () => {
             timeEnd: "2021-12-14T18:00:00.000Z",
             category: "illness"   
         })
-        .set("Authorization", `Bearer ${token}`)
+        .set("Authorization", `Bearer ${super_token}`)
         .expect(201)
 
         const resGetSelf = await request(app).get('/requestTimeOff/self')
-        .set("Authorization", `Bearer ${token}`)
+        .set("Authorization", `Bearer ${super_token}`)
         .expect(200)
         .expect((data) => {
             expect(data.body[0].day).toEqual(resPost.body.day);
@@ -109,7 +161,7 @@ describe("requestTimeOff testing", () => {
             timeEnd: "2021-12-14T18:00:00.000Z",
             category: "illness"   
         })
-        .set("Authorization", `Bearer ${token}`)
+        .set("Authorization", `Bearer ${super_token}`)
         .expect(201)
 
         const resPut = await request(app).put('/requestTimeOff/'+resPost.body._id)
@@ -117,11 +169,11 @@ describe("requestTimeOff testing", () => {
             reviewed: "true",
             accepted: "true"   
         })
-        .set("Authorization", `Bearer ${token}`)
+        .set("Authorization", `Bearer ${super_token}`)
         .expect(201)
 
         const resGetSelf = await request(app).get('/requestTimeOff/self')
-        .set("Authorization", `Bearer ${token}`)
+        .set("Authorization", `Bearer ${super_token}`)
         .expect(200)
         .expect((data) => {
             expect(data.body[0].reviewed).toEqual(true);
@@ -138,7 +190,7 @@ describe("requestTimeOff testing", () => {
             timeEnd: "2021-12-14T18:00:00.000Z",
             category: "illness"   
         })
-        .set("Authorization", `Bearer ${token}`)
+        .set("Authorization", `Bearer ${super_token}`)
         .expect(201)
 
         const resPut = await request(app).put('/requestTimeOff/'+resPost.body._id)
@@ -146,11 +198,11 @@ describe("requestTimeOff testing", () => {
             reviewed: "true",
             accepted: "false"   
         })
-        .set("Authorization", `Bearer ${token}`)
+        .set("Authorization", `Bearer ${super_token}`)
         .expect(201)
 
         const resGetSelf = await request(app).get('/requestTimeOff/self')
-        .set("Authorization", `Bearer ${token}`)
+        .set("Authorization", `Bearer ${super_token}`)
         .expect(200)
         .expect((data) => {
             expect(data.body[0].reviewed).toEqual(true);
